f965fa897ee6b527e2ac53e745fa4c98d035f034
NicoLellouche <nico.lellouche@gmail.com>
2017-09-30 16:05:29 -0400

    add query

---
+++ connexion_info.js
@@ -0,0 +1,22 @@
+/* All the database connexion information is in this file 
+
+#### FTP #####
+
+Host : 213.162.52.162
+Utilisateur : nippycar
+Mot de passe : Autre mail
+Mot de passe FTP : mu4UN6OhCa
+
+
+#### BDD ####
+
+Host :  localhost
+Utilisateur : test_nippycar
+Mot de passe : Autre mail
+Base : test_jato / test_nippycar
+
+PhpMyAdmin : http://phpmyadmin.cardata-vm-preprod.accelance.net/index.php
+Mot de passe BDD : 37xjbejxkxpppyu4
+
+
+*/
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var PORT = process.env.PORT || 3000;
 
 
 app.get('/', function (req, res) {
-	res.send('Car_data connexion set up...');
+	res.send('Car_data connexion setting up...');
 });
 
 app.listen(PORT, function(){
@@ -24,12 +24,12 @@ connection.connect(function(err) {
     console.error('error connecting: ' + err.stack);
     return;
   }
-  console.log('connected as id ' + connection.threadId);
+  console.log('connected as user ' + connection.user);
 });
  
-// connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-//   if (error) throw error;
-//   console.log('The solution is: ', results[0].solution);
-// });
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
+});
  
 connection.end();
